069d81d7804e28158b0a68d375f3e24ea7eb1f51
RostislavKis <rostislavkis@gmail.com>
2026-04-19 19:29:48 +0300

    chore: закрываю исходники — в репо остались только README и лого

--- rules/updater/update_lists.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Обновление списков маршрутизации
-# Скачивает geosite/geoip и конвертирует в формат nftables sets
-
-RULES_DIR="/etc/phoenix/rules"
-SOURCES_CONF="$(dirname "$0")/sources.conf"
-
-# TODO: чтение sources.conf, скачивание, конвертация
-echo "Обновление списков маршрутизации..."
